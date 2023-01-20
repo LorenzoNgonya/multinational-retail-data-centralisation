436aa6a55f3f1a4d7cab78d9cb46823c00b4bf06
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-01-20 03:27:53 +0000

    test

--- database_utils.py
+++ database_utils.py
@@ -2,8 +2,8 @@ import yaml
 
 class DatabaseConnector():
     def read_db_creds(self):
-        with open('cleardb_creds.yaml', 'r') as creds:
-            data = yaml.safe_load(creds)
+        with open('db_creds.yaml', 'r') as creds:
+            data = yaml.load(creds)
             
         return (data) 
         
