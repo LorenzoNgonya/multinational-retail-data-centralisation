e79c609369bda936273810bef090f3364db07d29
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-01-20 05:05:52 +0000

    continued code

--- database_utils.py
+++ database_utils.py
@@ -1,11 +1,26 @@
 import yaml
+from sqlalchemy import create_engine
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+import pandas as pd
 
 class DatabaseConnector():
     def read_db_creds(self):
-        with open('db_creds.yaml') as creds:
+        with open("db_creds.yaml", "r") as creds:
             data = yaml.safe_load(creds)
-            
-        print (data)
-
-
         
+        return data
+
+    def init_db_engine(self):
+        data = self.read_db_creds()
+        engine = create_engine(f"{data['DATABASE_TYPE']}+{data['DBAPI']}://{data['RDS_USER']}:{data['RDS_PASSWORD']}@{data['RDS_HOST']}:{data['RDS_PORT']}/{data['RDS_DATABASE']}")
+      
+        return engine
+
+    
+    def  list_db_tables (self):
+        engine = self.init_db_engine()
+        inspector = inspect(engine)
+        for table in inspector.get_table_names():
+            print(table)
