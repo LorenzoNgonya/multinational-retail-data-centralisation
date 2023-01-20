953e349ccc7bb54c96269609dc20203c5c3c290a
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-01-20 02:32:33 +0000

    initial commit

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,7 @@
-
+import yaml
 
 class DatabaseConnector():
+    def read_db_creds():
+        with open('db_creds.yaml', 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+            print(type(data_loaded))
