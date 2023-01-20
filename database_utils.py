59edd2d17fa70f5ccdce5a2d380c972d0d3522a7
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-01-20 02:47:37 +0000

    changes

--- database_utils.py
+++ database_utils.py
@@ -2,6 +2,6 @@ import yaml
 
 class DatabaseConnector():
     def read_db_creds():
-        with open('db_creds.yaml', 'r') as stream:
-            data_loaded = yaml.safe_load(stream)
-            print(type(data_loaded))
+        with open('../db_creds.yaml', 'r') as f:
+            data = yaml.safe_load(f)
+            print(data)
