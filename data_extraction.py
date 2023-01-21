76c3df610eef397e7824cb0d79ec574f7833c468
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-01-21 06:55:12 +0000

    updates code

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,9 @@
-
+from database_utils import DatabaseConnector
+import pandas as pd
 
 class DataExtractor():
-    
+    def read_rds_table(self, database, table='legacy_users'):
+        eng = dbcon.init_db_engine()
+    user_tb = dbcon.list_db_tables()
+    users = pd.read_sql_table(user_tb[1],eng)
+    return users
--- database_utils.py
+++ database_utils.py
@@ -1,6 +1,5 @@
 import yaml
 from sqlalchemy import create_engine
-from sqlalchemy import create_engine
 from sqlalchemy import inspect
 
 
