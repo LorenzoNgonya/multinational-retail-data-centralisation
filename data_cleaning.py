7e156b2a117c6ee9ece3f0b0ab798376a26b091d
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-01-22 06:37:58 +0000

    updating code

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,4 +1,14 @@
-
+import pandas as pd
+from data_extraction import DataExtractor
+import tabula
+import yaml
 
 class DataClean():
-    
+    def clean_user_data(self):
+        df = DataExtractor().extract_rds_table('legacy_users')
+        df['date_of_birth'] = pd.to_datetime(df['date_of_birth'],  infer_datetime_format=True, errors='coerce')
+        df['join_date'] = pd.to_datetime(df['join_date'], infer_datetime_format=True, errors='coerce')
+        #print(df['date_of_birth'])
+        df.dropna(subset=['date_of_birth','join_date'], inplace=True)
+
+   
--- data_extraction.py
+++ data_extraction.py
@@ -1,9 +1,14 @@
+import yaml
+from sqlalchemy import create_engine
 from database_utils import DatabaseConnector
 import pandas as pd
+import tabula
 
 class DataExtractor():
-    def read_rds_table(self, database, table='legacy_users'):
+    def read_rds_table (self,dbcon = DatabaseConnector()):
         eng = dbcon.init_db_engine()
-    user_tb = dbcon.list_db_tables()
-    users = pd.read_sql_table(user_tb[1],eng)
-    return users
+        user_tb = dbcon.list_db_tables()
+        users = pd.read_sql_table(user_tb[1],eng)
+        print (users)
+
+ 
