852b03f28b4bba4594c5fec04916522f0fd4d162
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-02-06 16:24:20 +0000

    work

--- data_extraction.py
+++ data_extraction.py
@@ -3,5 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy import inspect
 import pandas as pd
 class DataExtractor:
-    def list_db_tables(self):
-     pass
+    def read_rds_table(self, db_con = DatabaseConnector()):
+     engine1 = db_con.init_db_engine()
+     user_tb = db_con.list_db_tables()
+     user = pd.read_sql_table(user_tb[1],engine1)
+     print(user.head)
+     return user
+
+if __name__ == "__main__":
+    ex = DataExtractor()
+    ex.read_rds_table()
