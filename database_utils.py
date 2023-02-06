02fcd7daf5b4f02e8c97bfc9034b229e33d0573e
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-02-06 16:24:11 +0000

    working code

--- database_utils.py
+++ database_utils.py
@@ -10,9 +10,24 @@ class DatabaseConnector:
     def read_db_creds(self):
         with open('db_creds.yaml', 'r') as f:
             data = yaml.safe_load(f)
-            print(data)    
+            #print(data)    
         return data
 
-db = DatabaseConnector()
-db.read_db_creds
-    
+    def init_db_engine(self):
+        creds = self.read_db_creds()
+        engine = create_engine(f"{'postgresql'}+{creds['DBAPI']}://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}")
+        return engine 
+
+    def list_db_tables(self):
+        eng = self.init_db_engine()
+        eng.connect()
+        inspector = inspect(eng)
+        return inspector.get_table_names()     
+
+
+if __name__ == "__main__":
+    db = DatabaseConnector()
+    db.read_db_creds()
+    db.init_db_engine()
+    tables = db.list_db_tables()
+    print(tables)
