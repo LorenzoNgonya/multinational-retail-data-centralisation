4906af0c125515016e381f8d484d4ff1795ce71d
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2023-02-14 15:25:39 +0000

    reads pdf

--- data_extraction.py
+++ data_extraction.py
@@ -10,13 +10,11 @@ class DataExtractor:
      print(user.head)
      return user
 
-    def retrieve_pdf_data(self,link):
-        link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
-        dfs = tabula.read_pdf(link, pages='all', multiple_tables=True)
-        print(dfs)
-        #return dfs
+    def retrieve_pdf_data(self, address='card_details.pdf'):
+        dfs = tabula.read_pdf(address, pages='all')
+        return dfs
 
 
 if __name__ == "__main__":
     ex = DataExtractor()
-    ex.retrieve_pdf_data(link=any)
+    ex.retrieve_pdf_data()
